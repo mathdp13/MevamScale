d8f28adcfd9717de1c98d61f74c86b1592d3bee9
Matheus de Paula Silva <sillva130@gmail.com>
2026-05-04 14:57:55 -0300

    feat: rota /ministerio/:id no App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Login from './pages/Login';
 import Registro from './pages/Registro';
 import Perfil from './pages/Perfil';
 import Home from './pages/Home';
+import Ministerio from './pages/Ministerio';
 
 const PrivateRoute = ({ children }) => {
   const token = localStorage.getItem('token');
@@ -18,6 +19,7 @@ function App() {
       <Route path="/home" element={<PrivateRoute><Home /></PrivateRoute>} />
       <Route path="/ministerios" element={<PrivateRoute><Home /></PrivateRoute>} />
       <Route path="/perfil" element={<PrivateRoute><Perfil /></PrivateRoute>} />
+      <Route path="/ministerio/:id" element={<PrivateRoute><Ministerio /></PrivateRoute>} />
       <Route path="/escalas" element={<PrivateRoute><div className="text-white p-10">Tela de Escalas em breve...</div></PrivateRoute>} />
 
       <Route path="*" element={<Navigate to="/" />} />
